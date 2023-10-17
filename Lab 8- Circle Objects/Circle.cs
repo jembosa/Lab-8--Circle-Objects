64fa0a49764ec974b26fa88a8cb695aaddb65c89
Alex Young <alexanderjyoung15@gmail.com>
2023-10-17 08:40:22 -0400

    Add project files.

---
+++ Lab 8- Circle Objects/Circle.cs
@@ -0,0 +1,46 @@
+﻿namespace Lab_8__Circle_Objects
+{
+    public class Circle
+    {
+        public double Radius { get; set; }
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+
+        public double CalculateDiameter()
+        {
+
+            return 2 * Radius;
+        }
+
+        public double CalculateCircumference()
+        {
+            return 2 * Math.PI * Radius;
+        }
+
+        public double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+
+        public double GetRadius()
+        {
+            return Radius;
+        }
+
+        public void Grow()
+        {
+            Radius = Radius * 2;
+        }
+    }
+}
+
+
+
+
+
+
+
+
---
+++ Lab 8- Circle Objects/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace Lab_8__Circle_Objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius = 0;
+
+            while (true)
+            {
+                while (true)
+                {
+
+
+
+                    Console.WriteLine("Please enter radius of circle: ");
+                    if (double.TryParse(Console.ReadLine(), out radius))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input, Please enter valid number");
+                    }
+
+                }
+                Circle circle = new Circle(radius);
+
+                bool grow = true;
+
+                while (grow)
+                {
+                    Console.WriteLine($"Diameter: {circle.CalculateDiameter()}");
+                    Console.WriteLine($"Circumference: {circle.CalculateCircumference()}");
+                    Console.WriteLine($"Area: {circle.CalculateArea()}");
+
+                    Console.WriteLine("Would you like the circle to grow? 'y/n'");
+                    string response = Console.ReadLine();
+
+                    if (response.ToLower() == "y")
+                    {
+                        circle.Grow();
+                        Console.WriteLine("Kooloo-Limpah! The circle has Grown!");
+                        Console.WriteLine($"New Radius: {circle.GetRadius()}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Goodbye. The circles final radius is: {circle.GetRadius()}");
+                        grow = false;
+                    }
+                }
+                Console.WriteLine("Do you want to start over? y/n");
+                string startover = Console.ReadLine();
+                if (startover.ToLower() != "y")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
+
